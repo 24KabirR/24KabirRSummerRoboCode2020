8735a85df1ca74da8c40a1a87589daf1356cc5a5
CMEONE <contact@kabirwearsato.ga>
2020-08-11 17:22:09 -0700

    Added Drivetrain to WestCoastManual

--- src/main/java/frc/robot/commands/drivetrain/WestCoastManual.java
+++ src/main/java/frc/robot/commands/drivetrain/WestCoastManual.java
@@ -5,7 +5,13 @@ import frc.robot.subsystems.Drivetrain;
 
 public class WestCoastManual extends harkerrobolib.commands.drivetrain.WestCoastManaul{
     public WestCoastManual(){
-        
+        super(Drivetrain.getInstance(), 
+        Drivetrain.DRIVE_VELOCITY_SLOT, Drivetrain.ANGLE_POSITION_SLOT, 
+        OI.getInstance().getDriverGamepad(), 
+        Drivetrain.MAX_DRIVE_VELOCITY, Drivetrain.MAX_ROTATION_VELOCITY, 
+        Drivetrain.PIGEON_kP, 
+        OUTPUT_MULTIPLIER);
+    
     }
 
 
